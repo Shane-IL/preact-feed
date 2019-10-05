63e2179189d283ded4a6d46b911dc6457d7f5da0
Shane <shanegoodson101@gmail.com>
2019-10-05 21:21:23 +0300

    some webpack tweaking

--- preact-feed-original/webpack.config.js
+++ preact-feed-original/webpack.config.js
@@ -3,100 +3,101 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV !== 'production'
-
-module.exports = {
-	mode: 'development',
-
-	entry: './src/index.js',
-
-	output: {
-		filename: '[name].[chunkhash].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-
-	plugins: [
-		new webpack.ProgressPlugin(), 
-		new HtmlWebpackPlugin(), 
-		new MiniCssExtractPlugin({
-			filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-			chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-		})
-	],
-
-	module: {
-		rules: [
-			{
-				test: /.(js|jsx)$/,
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
-
-				options: {
-					plugins: [
-						['syntax-dynamic-import'], 
-						['@babel/plugin-proposal-class-properties'], 
-						["@babel/plugin-transform-react-jsx", {
-							"pragma": "h",
-							"pragmaFrag": "Fragment",
-						  }]
-					],
-
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
+module.exports = (env, argv) => {
+	const isDevelopment = argv.mode !== 'production'
+	return {
+		mode: argv.mode,
+	
+		entry: './src/index.js',
+	
+		output: {
+			filename: '[name].[chunkhash].js',
+			path: path.resolve(__dirname, 'dist')
+		},
+	
+		plugins: [
+			new webpack.ProgressPlugin(), 
+			new HtmlWebpackPlugin(), 
+			new MiniCssExtractPlugin({
+				filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+				chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+			})
+		],
+	
+		module: {
+			rules: [
+				{
+					test: /.(js|jsx)$/,
+					include: [path.resolve(__dirname, 'src')],
+					loader: 'babel-loader',
+	
+					options: {
+						plugins: [
+							['syntax-dynamic-import'], 
+							['@babel/plugin-proposal-class-properties'], 
+							["@babel/plugin-transform-react-jsx", {
+								"pragma": "h",
+								"pragmaFrag": "Fragment",
+							  }]
+						],
+	
+						presets: [
+							[
+								'@babel/preset-env',
+								{
+									modules: false
+								}
+							]
 						]
+					}
+				},
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.module\.s(a|c)ss$/,
+					loader: [
+						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true,
+								sourceMap: isDevelopment
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: isDevelopment
+							}
+						}
+					]
+				},
+				{
+					test: /\.s(a|c)ss$/,
+					exclude: /\.module.(s(a|c)ss)$/,
+					loader: [
+						isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+						'css-loader',
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: isDevelopment
+							}
+						}
 					]
 				}
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.module\.s(a|c)ss$/,
-				loader: [
-					isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true,
-							sourceMap: isDevelopment
-						}
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: isDevelopment
-						}
-					}
-				]
-			},
-			{
-				test: /\.s(a|c)ss$/,
-				exclude: /\.module.(s(a|c)ss)$/,
-				loader: [
-					isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-					'css-loader',
-					{
-						loader: 'sass-loader',
-						options: {
-							sourceMap: isDevelopment
-						}
-					}
-				]
-			}
-		]
-	},
-	devtool: isDevelopment ? "inline-sourcemap" : false,
-	devServer: {
-		compress: true,
-		open: true,
-		port: 5000
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.css', '.sass']
+			]
+		},
+		devtool: isDevelopment ? "inline-sourcemap" : false,
+		devServer: {
+			compress: true,
+			open: true,
+			port: 5000
+		},
+		resolve: {
+			extensions: ['.js', '.jsx', '.css', '.sass']
+		}
 	}
 };
